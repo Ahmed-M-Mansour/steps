914cd1fc51cc9977334fb28aa7743310a3cf743d
Ahmed Mansour <58402781+Ahmed-M-Mansour@users.noreply.github.com>
2024-05-20 23:35:01 +0300

    Finished the steps task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [isopen, setOpen] = useState(false);
+
+  const next = () => step < 3 && setStep((s) => s + 1);
+  const prev = () => step > 1 && setStep((s) => s - 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className="close" onClick={() => setOpen(!isopen)}>
+        &times;
+      </button>
+      {isopen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}> 1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}> 2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}> 3</div>
+          </div>
+
+          <p className="message">
+            Step {step} {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              onClick={prev}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            >
+              Pervious
+            </button>
+            <button
+              onClick={next}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
